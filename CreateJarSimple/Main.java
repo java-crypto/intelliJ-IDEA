a5c245e2b5eed13e3a65c65a7e2a9e400f79c944
Michael Fehr <javacrypto@gmx.de>
2020-03-31 17:05:16 +0200

    Add files via upload

---
+++ CreateJarSimple/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Herkunft/Origin: http://java-crypto.bplaced.net/
+ * Programmierer/Programmer: Michael Fehr
+ * Copyright/Copyright: frei verwendbares Programm (Public Domain)
+ * Copyright: This is free and unencumbered software released into the public domain.
+ * Lizenttext/Licence: <http://unlicense.org>
+ * getestet mit/tested with: Java Runtime Environment 11.0.5 x64
+ * verwendete IDE/used IDE: intelliJ IDEA 2019.3.1
+ * Datum/Date (dd.mm.jjjj): 31.12.2019
+ * Funktion: intelliJ JAR Erzeugung Einfach
+ * Function: intelliJ JAR creation simple
+ *
+ * Hinweis/Notice
+ * Sie benoetigen intelliJ um das Programm uebersetzen und ausfuehren zu koennen
+ * You need intelliJ to build and run the program
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("intelliJ IDEA JAR Erzeugung Einfach");
+    }
+}
---
+++ CreateJarSimple/intellijCreateJarSimple.bat
@@ -0,0 +1,2 @@
+java -jar intellijCreateJarSimple.jar
+pause
